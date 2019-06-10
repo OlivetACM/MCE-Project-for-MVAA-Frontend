b8577a2f2a55daee879ac1fd8cf195bee1465ca0
Joshua Hufford <joshuamh1135@gmail.com>
2019-06-10 14:05:20 -0400

    Fix site to delete data after refresh

--- www/home/views.py
+++ www/home/views.py
@@ -1,31 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
 
-from flask import Flask, request
-app = Flask(__name__)
 
 import requests
 
-data = {}
+data = None
 
-
+@csrf_exempt
 def index(request):
-    return render(request, 'index.html', context={"data": data})
+    global data
+    if request.method == 'POST':
+        data = str(request.body.decode('utf-8'))
+        data = data[2: -1]
+    elif request.method == 'GET':
+        display_data = data
+        data = ""
+        return render(request, 'index.html', context={"data": display_data})
+
+    return render(request, 'index.html', context={"data": ""})
 
 
 def submit(request):
+    global data
+    data = ""
     course_code = request.POST['course_code']
     web_hook_url = "http://127.0.0.1:5001"
 
     r = requests.post(web_hook_url, course_code)
 
     return HttpResponseRedirect('/')
-
-
-# @app.route('/', methods=['POST'])
-# def web_hook():
-#     if request.method == 'POST':
-#         global data
-#         data = request.data
