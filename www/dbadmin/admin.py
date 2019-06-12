40f8dc8b195df86627d7e2c105ca7697f2d380a7
xejnayr <xejnayr@gmail.com>
2019-06-12 13:46:13 -0400

    Added Institutions, Reviewers, and Outcomes to admin site

--- www/dbadmin/admin.py
+++ www/dbadmin/admin.py
@@ -1,7 +1,7 @@
 # from django.contrib import AdminSite
 # from django.utils.translation import ugettext_lazy
 from django.contrib import admin
-from dbadmin.models import Course
+from dbadmin.models import Course, Outcome, Reviewer, Institution
 
 
 # class MyAdminSite(AdminSite):
@@ -10,13 +10,32 @@ from dbadmin.models import Course
 #     index_title = ugettext_lazy('Olivet MCE Database Administration')
 
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ("CourseID", "CourseNumber", "CourseName", "CourseDescription", "CourseCredit",
+    list_display = ("CourseNumber", "CourseName", "CourseDescription", "CourseCredit",
                     "CourseEquivalenceNonOC", "InstitutionID", "ReviewerID")
     search_fields = ("CourseNumber",)
 
 
+class OutcomeAdmin(admin.ModelAdmin):
+    list_display = ("CourseNumber", "OutcomeDescription")
+    search_fields = ("CourseNumber", "OutcomeDescription")
+
+
+class ReviewerAdmin(admin.ModelAdmin):
+    list_display = ("ReviewerID", "ReviewerName", "ReviewerPhone", "ReviewerEmail", "ReviewerDepartment")
+    search_fields = ("ReviewerName", "ReviewerDepartment")
+
+
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ("InstitutionName", "InstitutionAddress", "InstitutionCity", "InstitutionState",
+                    "InstitutionZipcode", "InstitutionWebSite")
+    search_fields = ("InstitutionName",)
+
+
 # admin_site = MyAdminSite()
 admin.site.register(Course, CourseAdmin)
+admin.site.register(Outcome, OutcomeAdmin)
+admin.site.register(Reviewer, ReviewerAdmin)
+admin.site.register(Institution, InstitutionAdmin)
 
 
 # Register your models here.
