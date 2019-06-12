b10a855f24a9c5ab6fbc78e967a4efb96accb0ac
Joshua Hufford <joshuamh1135@gmail.com>
2019-06-12 12:06:44 -0400

    Pretty website

--- www/home/form.py
+++ www/home/form.py
@@ -1,10 +1,14 @@
+import logging
 import sqlite3
 
 from django import forms
 
+logging.basicConfig(filename='mce.log', level=logging.ERROR)
+
 
 class CourseForm(forms.Form):
     course_code = forms.CharField(max_length=20)
+
 
 class CourseLookup:
     def __init__(self, course):
@@ -12,32 +16,52 @@ class CourseLookup:
         self.conn = sqlite3.connect('db.sqlite3', check_same_thread=False)
         self.cursor = self.conn.cursor()
 
-    def get_equivalent_courses(self, course_code):
-        self.cursor.execute('''SELECT * FROM course WHERE CourseNumber = ? and CourseEquivalenceNonOC not NULL;''', [course_code])
-        results = self.cursor.fetchall()
-        return str(self.format_results(results)).replace("'", '"')
+    def get_equivalent_courses(self):
+        try:
+            self.cursor.execute(
+                '''
+                    SELECT * FROM dbadmin_course WHERE CourseNumber = ? and CourseEquivalenceNonOC not NULL;
+                ''', [self.course])
+            results = self.cursor.fetchall()
+            return str(self.format_results(results)).replace("'", '"')
+        except sqlite3.Error as e:
+            logging.error("Error occurred while getting equivalent courses: ", e)
+            return ""
 
-    def get_oc_course_description(self, course_code):
-        self.cursor.execute('''SELECT CourseDescription FROM course WHERE CourseNumber = ?''', [course_code])
-        return self.cursor.fetchall()
+    def get_oc_course_description(self, oc_course_code):
+        try:
+            self.cursor.execute(
+                '''
+                    SELECT CourseDescription FROM dbadmin_course WHERE CourseNumber = ?
+                ''', [oc_course_code])
+            return self.cursor.fetchall()
+        except sqlite3.Error as e:
+            logging.error("Error occurred while getting OC course description: ", e)
+            return ""
 
     def format_results(self, data_set):
-        data_set = str(data_set)[1:-1].split('), (')
-        list_data = []
+        try:
+            data_set = str(data_set)[1:-1].split('), (')
+            list_data = []
+            print(data_set)
 
-        for data in data_set:
-            dict_data = {}
-            new_data = data.replace('(', '').replace(')', "").replace("'", "")
-            split_data = new_data.split(', ')
+            for data in data_set:
+                dict_data = {}
+                new_data = data.replace('(', '').replace(')', "").replace("'", "")
+                split_data = new_data.split(', ')
 
-            dict_data["CourseID"] = split_data[0]
-            dict_data["CourseNumber"] = split_data[1]
-            dict_data["CourseName"] = split_data[2]
-            dict_data["CourseDescription"] = str(split_data[3:-4])[1:-1].replace("', '", ", ").replace("'", "")
-            dict_data["CourseCredit"] = split_data[-3]
-            dict_data["CourseEquivalenceNonOC"] = split_data[-2]
-            dict_data["CourseDescriptionNonOC"] = str(self.get_oc_course_description(split_data[-2]))[3:-4]
-            dict_data["InstitutionID"] = split_data[-1]
-            list_data.append(dict_data)
+                dict_data["CourseID"] = split_data[0]
+                dict_data["CourseNumber"] = split_data[1]
+                dict_data["CourseName"] = split_data[2]
+                dict_data["CourseDescription"] = str(split_data[3:-4])[1:-1].replace("', '", ", ").replace("'", "")
+                dict_data["CourseCredit"] = split_data[-3]
+                dict_data["CourseEquivalenceNonOC"] = split_data[-2]
+                dict_data["CourseDescriptionNonOC"] = str(self.get_oc_course_description(split_data[-2]))[3:-4]
+                dict_data["InstitutionID"] = split_data[-1]
+                list_data.append(dict_data)
 
-        return list_data
+            return list_data
+
+        except IndexError as e:
+            logging.error("Course was not found: ", e)
+            return ""
--- www/home/views.py
+++ www/home/views.py
@@ -1,66 +1,22 @@
-from django.shortcuts import render, render_to_response
-from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 from django.views.decorators.csrf import csrf_exempt
-
-
-import requests
 
 from .form import CourseForm, CourseLookup
 
+
+@csrf_exempt
 def index(request):
     if request.method == 'POST':
         form = CourseForm(request.POST)
+        data = ""
         if form.is_valid():
-            CourseLookup(form.cleaned_data['course_code'])
-            return HttpResponseRedirect('/')
+            course_lookup = CourseLookup(form.cleaned_data['course_code'])
+            data = course_lookup.get_equivalent_courses()
+
     else:
         form = CourseForm()
+        data = ""
 
-    return render(request, 'index.html', {'form': form})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# data = None
-
-# @csrf_exempt
-# def index(request):
-#     global data
-#     if request.method == 'POST':
-#         data = str(request.body.decode('utf-8'))
-#         data = data[2: -1]
-#     elif request.method == 'GET':
-#         display_data = data
-#         data = ""
-#         return render(request, 'index.html', context={"data": display_data})
-#
-#     return render(request, 'index.html', context={"data": ""})
-#
-#
-# def submit(request):
-#     global data
-#     data = ""
-#     course_code = request.POST['course_code']
-#     web_hook_url = "http://127.0.0.1:5001"
-#
-#     r = requests.post(web_hook_url, course_code)
-#
-#     return HttpResponseRedirect('/')
+    return render_to_response('index.html', {'form': form, 'data': data}, RequestContext(request))
