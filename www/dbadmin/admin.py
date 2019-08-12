f99c7eed57eb9f71d678623a75023492a7692472
David Lewis <dlewis@gmail.com>
2019-08-12 14:30:07 -0400

    attempting to secure custom view

--- www/dbadmin/admin.py
+++ www/dbadmin/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.admin import AdminSite
+from django.http import HttpResponse
 from dbadmin.models import Course, Outcome, Reviewer, Institution
+
+
 
 
 # list_display = what we want shown in the admin site
@@ -36,9 +40,32 @@ admin.site.register(Institution, InstitutionAdmin)
 
 
 # Register your models here.
-@admin.site.register_view('equivalency', urlname='equivalency', name='Generate Equivalency Review')
-def equivalency(request):
-    context = dict(
-        admin.site.each_context(request),
-    )
-    return render_to_response(request, 'mce/admin/equivalency.html', context)
+
+# def equivalency(request):
+#     context = dict(
+#         admin.site.each_context(request),
+#     )
+#     return render_to_response(request, 'mce/admin/equivalency.html', context)
+#
+#
+
+#
+class MyAdminSite(AdminSite):
+
+    def get_urls(self):
+        from django.urls import path
+        urls = super().get_urls()
+        urls += [path('equivalency/', self.admin_view(self.equivalency))]
+        return urls
+
+    def equivalency(request):
+        context = dict(
+            admin.site.each_context(request),
+        )
+        return render_to_response(request, 'mce/admin/equivalency.html', context)
+
+
+
+admin_site = MyAdminSite()
+
+admin.site.register_view('equivalency', urlname='equivalency', name='Generate Equivalency Review', view=admin_site)
